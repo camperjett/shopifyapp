49521f1cb95e554803d4bac8a4e164ee17eb7400
Jaideep Sharma <jaideep.sharma.cse20@itbhu.ac.in>
2021-09-19 10:24:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Sync your customers</h1>
+      <button id="myButton" className="float-left submit-button">Enable Workflow</button>
+    </div>
+  );
+}
+
+export default App;

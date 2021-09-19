193335da16caf4730c841e260e1c70c89a8adfd3
Jaideep Sharma <jaideep.sharma.cse20@itbhu.ac.in>
2021-09-19 16:37:50 +0530

    added axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import axios from "axios";
+
 function App() {
   function auth() {
-    window.location.href = "https://api.thecodemesh.online/api/v1/enable/user-workflow/6146d058c0dc290012eb4d6d/" + window.location.search;
+    const url = "https://api.thecodemesh.online/api/v1/enable/user-workflow/6146d058c0dc290012eb4d6d/" + window.location.search;
+    axios.get(url)
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log("error", error);
+      });
   }
   return (
     <div>
